6a2545c4c80b24df0c3e2bef0155b524f74c1ede
Matsumoto Yoichiro <metasta@gmail.com>
2019-03-27 17:14:45 +0900

    create OTF branch

--- bin/adjust-linegap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -
-#
-# usage: adujst-linegap.sh < features
-sed -e 's/ Descender [-0-9]*/ Descender -370/g' \
-    -e 's/winDescent [0-9]*/winDescent 370/g' \
-    -e 's/VertTypoLineGap [0-9]*/VertTypoLineGap 1000/g'
